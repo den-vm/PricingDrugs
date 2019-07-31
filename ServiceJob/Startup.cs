90714004ae142a933f8b52d508b4b5cb9fd0ebda
Денис <layon17@outlook.com>
2019-07-31 17:25:11 +0800

    Delete space Startup.cs

--- ServiceJob/Startup.cs
+++ ServiceJob/Startup.cs
@@ -14,7 +14,6 @@ namespace ServiceJob
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
         }
 
         public IConfiguration Configuration { get; }
