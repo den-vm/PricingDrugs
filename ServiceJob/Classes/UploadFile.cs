d7144df84d6c6b4a899a1f524992228c2fd8073b
Денис <layon17@outlook.com>
2019-09-03 01:04:37 +0800

    add adapter XLSX

--- ServiceJob/Classes/UploadFile.cs
+++ ServiceJob/Classes/UploadFile.cs
@@ -9,7 +9,24 @@ namespace ServiceJob.Models
 
         public void ReaderFileJvnlp(IFormFile file)
         {
+            file.
+            ////Создаём приложение.
+            //Microsoft.Office.Interop.Excel.Application ObjExcel = new Microsoft.Office.Interop.Excel.Application();
+            ////Открываем книгу.                                                                                                                                                        
+            //Microsoft.Office.Interop.Excel.Workbook ObjWorkBook = ObjExcel.Workbooks.Open(NameExcel, 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+            ////Выбираем таблицу(лист).
+            //Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
+            //ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets[1];
+            //string[,] Telephons = new string[100];
+            ////Выбираем первые сто записей из столбца.
+            //for (int i = 1; i < 101; i++)
+            //{
+            //    //Выбираем область таблицы. (в нашем случае просто ячейку)
+            //    Microsoft.Office.Interop.Excel.Range range = ObjWorkSheet.get_Range("F" + i.ToString(), "F" + i.ToString());
+            //    //Добавляем полученный из ячейки текст.
+            //    Telephons[i - 1] = range.Text.ToString();
 
+            //}
         }
     }
 }
