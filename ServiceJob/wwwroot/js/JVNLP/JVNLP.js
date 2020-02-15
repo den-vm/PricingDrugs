437c2a64f79291fa693ef67ed7af093bcf5d9114
Денис <layon17@outlook.com>
2020-02-16 04:00:03 +0800

    Created table form criteria to price drugs

--- ServiceJob/wwwroot/js/JVNLP/JVNLP.js
+++ ServiceJob/wwwroot/js/JVNLP/JVNLP.js
@@ -1,16 +1,27 @@
 ﻿var visibleFormfile = false;
 var visibleFormTableDrugs = false;
+var visibleFormDrugsPriceCriteria = false;
 
-$("li[name=openFormFile]").click(function() {
-    if (visibleFormTableDrugs)
+$("li[name=openFormFile]").click(function () {
+    if (visibleFormTableDrugs) // закрыть форму наркотических препаратов
         FormTableDrugs(100);
+    if (visibleFormDrugsPriceCriteria) // закрыть форму таблица цен
+        FormCalculationCriteria(100);
     Formfile();
-
 });
-$("li[name=openFormTableDrugs]").click(function() {
-    if (visibleFormfile)
+$("li[name=openFormTableDrugs]").click(function () {
+    if (visibleFormfile) // закрыть форму загрузки препаратов
         Formfile(100);
+    if (visibleFormDrugsPriceCriteria) // закрыть форму таблица цен
+        FormCalculationCriteria(100);
     FormTableDrugs(100);
+});
+$("li[name=openFormDrugsPriceCriteria]").click(function () {
+    if (visibleFormfile) // закрыть форму загрузки препаратов
+        Formfile(100);
+    if (visibleFormTableDrugs) // закрыть форму наркотических препаратов
+        FormTableDrugs(100);
+    FormCalculationCriteria(100);
 });
 
 function Formfile(speed = 200) {
@@ -18,11 +29,11 @@ function Formfile(speed = 200) {
     $("div[name=lockbody]").slideToggle({
         duration: (speed + 100),
         easing: "linear",
-        always: function() {
+        always: function () {
             $("div[name=divJvnlp]").slideToggle({
                 duration: speed,
                 easing: "linear",
-                always: function() {
+                always: function () {
                     if ($("div[name=divJvnlp]").css("display") === "block") visibleFormfile = true;
                     else visibleFormfile = false;
                 }
@@ -36,12 +47,35 @@ function FormTableDrugs(speed = 200) {
     $("div[name=lockbody]").slideToggle({
         duration: (speed + 100),
         easing: "linear",
-        always: function() {
+        always: function () {
             $("div[name=divDrugsDownload]").slideToggle({
                 duration: speed,
                 easing: "linear",
-                always: function() {
+                always: function () {
                     getDrugs();
+                }
+            });
+        }
+    });
+}
+
+// получить таблицу ценовых критериев на препараты
+function FormCalculationCriteria(speed = 200) {
+    $("div[name=lockbody]").slideToggle({
+        duration: (speed + 100),
+        easing: "linear",
+        always: function () {
+            $("div[name=divDrugsPriceCriteria]").slideToggle({
+                duration: speed,
+                easing: "linear",
+                always: function () {
+                    //получить коэфиценты для расчёта цен
+                    if ($("div[name=divDrugsPriceCriteria]").css("display") === "block") {
+                        visibleFormDrugsPriceCriteria = true;
+                        // обратиться на сервер и получить критерии из файла
+                    } else {
+                        visibleFormDrugsPriceCriteria = false;
+                    }
                 }
             });
         }
@@ -69,21 +103,21 @@ function RequestFormNPDrugs(dataForm) {
         processData: false, // отключение преобразования строки запроса по contentType
         contentType:
             false, // отключение преобразования контента в тип по умолчанию: "application/x-www-form-urlencoded; charset=UTF-8"
-        success: function(data) {
+        success: function (data) {
             $.each(data["listmessages"],
-                function(key, message) {
+                function (key, message) {
                     var value = message.value;
                     if (value.typemessage === "error")
-                        setTimeout(function() {
-                                console.error(value["message"]);
-                                alertify.error(value["message"]);
-                            },
+                        setTimeout(function () {
+                            console.error(value["message"]);
+                            alertify.error(value["message"]);
+                        },
                             key * 200);
                     if (value.typemessage === "complite")
-                        setTimeout(function() {
-                                console.log(value["message"]);
-                                alertify.message(value["message"]);
-                            },
+                        setTimeout(function () {
+                            console.log(value["message"]);
+                            alertify.message(value["message"]);
+                        },
                             key * 200);
                     if (value.typemessage === "drugs") {
                         $("#RowDrugs").html(getDrugsHTML(value.message));
@@ -93,7 +127,7 @@ function RequestFormNPDrugs(dataForm) {
             var elementsDrugSave = $("tr[name=drugSave]")
                 .find($("input[name = nameDrug],input[name = dataDrugAdd],input[name = dataDrugDel]"));
             elementsDrugSave.on("input",
-                function() {
+                function () {
                     $(this).parents("td,tr").attr("name", "drugEdit");
                 });
         }
